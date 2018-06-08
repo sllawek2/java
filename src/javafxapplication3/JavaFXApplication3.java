9790237e1e3be602532981ed94d90885f169362e
sllawek2 <sllawek2@gmail.com>
2018-06-08 18:12:02 +0200

    usuniecie importow niepotrzebny

--- src/javafxapplication3/JavaFXApplication3.java
+++ src/javafxapplication3/JavaFXApplication3.java
@@ -17,11 +17,9 @@ import java.sql.*;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 /**
